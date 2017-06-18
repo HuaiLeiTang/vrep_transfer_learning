5c10eb1a5524f4061b28664aebd07db0ad74efc1
jonheng <jonheng@users.noreply.github.com>
2017-06-18 02:08:10 +0100

    Added image handling functions

--- image_utils/methods.py
+++ image_utils/methods.py
@@ -1,12 +1,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from skimage import exposure
-
+from skimage.util import random_noise
+from skimage.filters import gaussian, laplace, frangi, hessian
 
 def h5py_to_array(dataset, img_shape):
     # img_shape as a tuple
+    # assumes input is in the uint8 format
+    # converts into a standard float32 in range[0,1]
     images = np.array(dataset)
-    images = np.uint8(images)
+    images = np.float32(images)/255
     images = np.reshape(images, (images.shape[0],) + img_shape)
     return images
 
@@ -17,7 +20,7 @@ def convert_grayscale(images):
 
 def adjust_gamma(images, gamma):
     # gamma greater than 1 leads to darker images
-    # gamma less than 1 leads to brigher images
+    # gamma less than 1 leads to brighter images
     return exposure.adjust_gamma(images, gamma=gamma)
 
 def display(img):
@@ -28,6 +31,32 @@ def display_grayscale(img):
     plt.imshow(img, origin='lower', cmap=plt.cm.gray)
 
 
+def tint_images(images, filter=[1, 1, 1]):
+    return filter*images
+
+## noise function return images with values in range [0,1]
+def noise_gaussian(images, mean=0, var=0.01):
+    # adds a gaussian variable with specified mean and var
+    # to each pixel in image
+    return random_noise(images, mode='gaussian',
+                        mean=mean, var=var)
+
+def noise_sp(images, amount=0.05, salt_vs_pepper=0.5):
+    return random_noise(images, mode='s&p', amount=amount,
+                        salt_vs_pepper=salt_vs_pepper)
+
+def blur_gaussian(images, sigma=1):
+    return gaussian(images, sigma=sigma)
+
+def laplace_edges(images):
+    return laplace(images)
+
+def frangi_filter(image):
+    return frangi(image)
+
+def hessian_filter(image):
+    return hessian(image)
+
 # Test
 if __name__ == '__main__':
     print 'Testing image_utils/methods.py'
@@ -35,5 +64,58 @@ if __name__ == '__main__':
 
     file = h5py.File("../datasets/200iter100steps64res.hdf5", "r")
     images = file['images']
-    images = h5py_to_array(images, (64, 64, 3))
+    images = h5py_to_array(images[:5], (64, 64, 3))
+
+    print 'Displaying image'
     display(images[0])
+    plt.show()
+
+    print 'Displaying gamma=2 corrected image'
+    gamma_images = adjust_gamma(images,2)
+    display(gamma_images[0])
+    plt.show()
+
+    print 'Displaying yellow tinted image'
+    yellow_tinted_images = tint_images(images,[1,1,0])
+    display(yellow_tinted_images[0])
+    plt.show()
+
+    # var can be reduced to limit the effect
+    # default var=0.01
+    print 'Displaying gaussian noise image'
+    gaussian_noise_images = noise_gaussian(images)
+    display(gaussian_noise_images[0])
+    plt.show()
+
+    # s&p doesn't work properly for triple color channels
+    print 'Displaying salt and pepper noise image'
+    sp_images = noise_sp(images)
+    display(sp_images[0])
+    plt.show()
+
+    print 'Displaying gaussian blur image'
+    gaussian_blur_images = blur_gaussian(images, sigma=0.5)
+    display(gaussian_blur_images[0])
+    plt.show()
+
+    print 'Displaying laplace edges image'
+    laplace_images = laplace_edges(images)
+    display(laplace_images[0])
+    plt.show()
+
+    print 'Displaying grayscale image'
+    grayscale_images = convert_grayscale(images)
+    display_grayscale(grayscale_images[0])
+    plt.show()
+
+    # frangi and hessian filters can only be applied to one image at a time
+    # might not be an efficient operation
+    print 'Displaying frangi filtered image'
+    frangi_image = frangi(grayscale_images[0])
+    display_grayscale(frangi_image)
+    plt.show()
+
+    print 'Displaying hessian filtered image'
+    hessian_image = hessian(grayscale_images[0])
+    display_grayscale(hessian_image)
+    plt.show()
